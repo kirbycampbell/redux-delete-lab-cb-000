443491e6948c043967e8fd9eb919723a1ac76c68
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-15 20:23:37 +0000

    automatically backed up by learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,11 +3,21 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
+<<<<<<< HEAD
       const band = { id: Math.random(), name: action.name }
       return { ...state, bands: [...state.bands, band] }
     case 'DELETE_BAND':
       const bands = state.bands.filter(band => band.id !== action.id);
       return { bands };
+=======
+    const band = { id: Math.random(), name: action.name }
+      return { ...state, bands: [...state.bands, action.name] }
+
+    case 'DELETE_BAND':
+      const bands = state.bands.filter(band => band.id !== action.id);
+      return {bands };
+
+>>>>>>> bf97ce56b7b0ddf15de30a76547f963f40375b8d
     default:
       return state;
   }
